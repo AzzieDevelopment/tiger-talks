194c5daedc037377eedd4098790eff465d041b99
PD Nguyen <pnguye18@students.towson.edu>
2021-10-05 00:02:03 -0400

    initial commit with angular src/client dir and sign-up-form component

---
+++ src/client/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SignUpFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
