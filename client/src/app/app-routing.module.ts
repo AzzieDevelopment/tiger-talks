ad296dc47caac53d920402d54578ef41fde7520d
PD Nguyen <pnguye18@students.towson.edu>
2021-10-15 23:36:13 -0400

    skeleton of home page done, needs work on formatting

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
-  {path: 'home', component: SignUpFormComponent},
+  {path: 'home', component: HomePageComponent},
 ];
 
 @NgModule({
