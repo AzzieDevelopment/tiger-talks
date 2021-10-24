2224d83b5ceb1aa0df51151caea6f84b9b95937c
Dorian Brim <dbrim1@students.towson.edu>
2021-10-24 12:54:40 -0400

    Added 'IF NOT EXIST' clause to all commands

--- sql/up.sql
+++ sql/up.sql
@@ -1,6 +1,6 @@
-CREATE SCHEMA azziedev_tigertalk;
+CREATE SCHEMA IF NOT EXISTS azziedev_tigertalk;
 
-CREATE TABLE azziedev_tigertalk.user (
+CREATE TABLE IF NOT EXISTS azziedev_tigertalk.user (
     Id INT UNSIGNED NOT NULL,
     FirstName VARCHAR(64) NOT NULL,
     LastName VARCHAR(64) NOT NULL,
@@ -15,7 +15,7 @@ CREATE TABLE azziedev_tigertalk.user (
     PRIMARY KEY(Id)
     );
     
-CREATE TABLE azziedev_tigertalk.student (
+CREATE TABLE IF NOT EXISTS azziedev_tigertalk.student (
 	UserId INT UNSIGNED NOT NULL,
 	Major VARCHAR(32) NOT NULL,
 	Minor VARCHAR(32),
@@ -25,7 +25,7 @@ CREATE TABLE azziedev_tigertalk.student (
 	FOREIGN KEY(UserId) References azziedev_tigertalk.user(Id) ON DELETE CASCADE
 );
 
-CREATE TABLE azziedev_tigertalk.faculty (
+CREATE TABLE IF NOT EXISTS azziedev_tigertalk.faculty (
 	UserId INT UNSIGNED NOT NULL,
 	Title VARCHAR(32) NOT NULL,
     Department VARCHAR(32) NOT NULL,
@@ -33,7 +33,7 @@ CREATE TABLE azziedev_tigertalk.faculty (
 	FOREIGN KEY(UserId) References azziedev_tigertalk.user(Id) ON DELETE CASCADE
 );
 
-CREATE TABLE azziedev_tigertalk.tigerspace (
+CREATE TABLE IF NOT EXISTS azziedev_tigertalk.tigerspace (
 	Id INT UNSIGNED NOT NULL,
     UserId INT UNSIGNED NOT NULL,
     Title VARCHAR(64) NOT NULL,
@@ -43,7 +43,7 @@ CREATE TABLE azziedev_tigertalk.tigerspace (
     FOREIGN KEY(UserId) References azziedev_tigertalk.user(Id) ON DELETE CASCADE
 );
 
-CREATE TABLE azziedev_tigertalk.post (
+CREATE TABLE IF NOT EXISTS azziedev_tigertalk.post (
 	Id INT UNSIGNED NOT NULL,
     Title VARCHAR(64) UNIQUE NOT NULL,
     Body VarChar(2048) NOT NULL,
@@ -57,7 +57,7 @@ CREATE TABLE azziedev_tigertalk.post (
     FOREIGN KEY(TigerSpaceId) References azziedev_tigertalk.tigerspace(Id) ON DELETE CASCADE
 );
 
-CREATE TABLE azziedev_tigertalk.comment (
+CREATE TABLE IF NOT EXISTS azziedev_tigertalk.comment (
 	Id INT UNSIGNED NOT NULL,
     UserId INT UNSIGNED NOT NULL,
     PostId INT UNSIGNED NOT NULL,
@@ -69,21 +69,21 @@ CREATE TABLE azziedev_tigertalk.comment (
     FOREIGN KEY(PostId) References azziedev_tigertalk.post(Id) ON DELETE CASCADE
 );
 
-CREATE TABLE azziedev_tigertalk.follow (
+CREATE TABLE IF NOT EXISTS azziedev_tigertalk.follow (
 	UserId INT UNSIGNED NOT NULL,
     TigerSpaceId INT UNSIGNED NOT NULL,
     FOREIGN KEY(UserId) References azziedev_tigertalk.user(Id) ON DELETE CASCADE,
     FOREIGN KEY(TigerSpaceId) References azziedev_tigertalk.tigerspace(Id) ON DELETE CASCADE
 );
 
-CREATE TABLE azziedev_tigertalk.flaggedpost (
+CREATE TABLE IF NOT EXISTS azziedev_tigertalk.flaggedpost (
 	UserId INT UNSIGNED NOT NULL,
     PostId INT UNSIGNED NOT NULL,
     FOREIGN KEY(UserId) References azziedev_tigertalk.user(Id) ON DELETE CASCADE,
     FOREIGN KEY(PostId) References azziedev_tigertalk.post(Id) ON DELETE CASCADE
 );
 
-CREATE TABLE azziedev_tigertalk.flaggedcomment (
+CREATE TABLE IF NOT EXISTS azziedev_tigertalk.flaggedcomment (
 	UserId INT UNSIGNED NOT NULL,
     CommentId INT UNSIGNED NOT NULL,
     FOREIGN KEY(UserId) References azziedev_tigertalk.user(Id) ON DELETE CASCADE,
