ccaf2000ae3e8be909d8273862caa05c72219043
Samantha Hallcox <shallc1@students.towson.edu>
2021-10-31 00:14:16 -0400

    Fix route casing

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
-  { path: 'home', component: HomePageComponent },
-  { path: 'signup', component: SignUpFormComponent },
+  { path: 'Home', component: HomePageComponent },
+  { path: 'SignUp', component: SignUpFormComponent },
+  { path: 'SignIn', component: SignInComponent },
   { path: '**', redirectTo: '' }
 ];
 
