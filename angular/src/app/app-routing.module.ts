772e533ff2f87791c780a0099943d34e46262ef8
PD Nguyen <pnguye18@students.towson.edu>
2021-10-30 21:16:30 -0400

    merged pdTest and updatedFrontend

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
+
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
-
-const routes: Routes = [
-  {path: 'signin', component: SigninComponent},
-  {path: 'signup', component: SignupComponent}
-=======
-import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'home', component: HomePageComponent },
-  { path: 'signup', component: SignUpFormComponent },
+  { path: 'signin', component: SigninComponent},
+  { path: 'signup', component: SignupComponent },
   { path: '**', redirectTo: '' }
->>>>>>> 484d83b0d27560731f19d04c2d5a882b7a23bbdc
 ];
 
 @NgModule({
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,27 +4,19 @@ import { AngularMaterialModule } from './angular-material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-=======
-import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
-import { HomePageComponent } from './home-page/home-page.component';
->>>>>>> 484d83b0d27560731f19d04c2d5a882b7a23bbdc
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     SigninComponent,
-    SignupComponent
-=======
-    SignUpFormComponent,
+    SignupComponent,
     HomePageComponent
->>>>>>> 484d83b0d27560731f19d04c2d5a882b7a23bbdc
   ],
   imports: [
     BrowserModule,
