07d5444c7f3d73930e084f0e915049b1d77d6525
Samantha Hallcox <shallc1@students.towson.edu>
2021-10-30 22:08:18 -0400

    Updated signup form

--- angular/src/app/sign-up-form/sign-up-form.component.ts
+++ angular/src/app/sign-up-form/sign-up-form.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-sign-up-form',
   templateUrl: './sign-up-form.component.html',
   styleUrls: ['./sign-up-form.component.css']
 })
