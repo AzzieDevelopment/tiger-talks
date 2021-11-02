988f1589873a6a40fd350428268f1e1ea9568e49
Samantha Hallcox <shallc1@students.towson.edu>
2021-11-01 21:35:04 -0400

    Test connection between FE & BE

--- angular/src/app/home-page/home-page.component.ts
+++ angular/src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  message: string = "default message...";
+  
+  constructor(public appService: AppService) { }
 
   ngOnInit(): void {
+    this.appService.getHello().subscribe(
+      response => {
+        this.message = response.message;
+      },
+      error => {
+        console.log(error);
+        alert("Error: could not get message from backend.");
+      }
+    );
   }
 
 }
