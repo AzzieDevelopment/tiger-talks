da94399e39aca91a768e24ea9d80b462e4ca22c7
Samantha Hallcox <shallc1@students.towson.edu>
2021-11-06 14:51:58 -0400

    Add tigerspace-grid component and route

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { TigerSpacesGridComponent } from './tigerspaces-grid/tigerspaces-grid.component';
 
 const routes: Routes = [
   { path: '', component: HomePageComponent },
   { path: 'home', component: HomePageComponent },
   { path: 'signup', component: SignUpFormComponent },
   { path: 'signin', component: SignInComponent },
+  { path: 'tigerspaces', component: TigerSpacesGridComponent },
   { path: '**', redirectTo: '' }
 ];
 
---
+++ angular/src/app/tigerspaces-grid/tigerspaces-grid.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './tigerspaces-grid.component.html',
+  styleUrls: ['./tigerspaces-grid.component.css']
+})
+export class TigerSpacesGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
