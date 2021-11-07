019e88a1e59704991965eb86f9fb0d0113761fff
AzzieDev <18339356+AzzieDev@users.noreply.github.com>
2021-11-06 20:12:12 -0400

    Update db.js

--- express/db.js
+++ express/db.js
@@ -1,18 +1,20 @@
 const mysql = require('mysql');
+const globalvars = require("./global")
 
 // ============================================================
 // Database Connection Set Up
 // ============================================================
 
 const connection = mysql.createConnection({
-    host: '64.20.43.250',
-    user: 'azziedev_tigertalksdb',
-    password: 'NOX3-PJ]9i-s',
+    host: '64.20.43.250' ,
+    user: 'azziedev_tigertalksdb' ,
+    password: 'NOX3-PJ]9i-s' ,
     database: 'azziedev_tigertalks'
   })
   
   //Database connect status
   connection.connect((err) => {
+    console.log(globalvars());
     if (!err) {
       console.log("Connected");
     } else {
