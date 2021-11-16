524c5f6f20cb4d46cbfd0282e2bf3a76fb228df0
Sean Hicks <55099527+Sean-Hicks@users.noreply.github.com>
2021-11-16 13:21:47 -0500

    add posts to home page

--- angular/src/app/home-page/recent-posts/recent-posts.component.ts
+++ angular/src/app/home-page/recent-posts/recent-posts.component.ts
@@ -1,4 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+export class posts {
+  constructor(
+    public Id: number,
+    public Title: string,
+    public Body: string,
+    public Category: string,
+    public Upvotes: number,
+    public Timestamp: string,
+    public UserId: string,
+    public TigerSpaceId: number
+){
+}
+}
 
 @Component({
   selector: 'recent-posts',
@@ -7,9 +22,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecentPostsComponent implements OnInit {
 
-  constructor() { }
+  post!:posts[];
+
+  constructor(
+    private httpClient:HttpClient
+  ) { }
 
   ngOnInit(): void {
+    this.getPosts();
+  }
+
+  getPosts(){
+    this.httpClient.get<any>('/api/getrecentposts').subscribe(
+      response => {
+        console.log(response);
+        this.post=response;
+      }
+    )
   }
 
 }
