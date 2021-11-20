e6d986d939c81126b79435b366ddb6bc67011846
PD Nguyen <pnguye18@students.towson.edu>
2021-11-19 22:12:28 -0500

    changed make-post component to use Bootstrap

--- angular/src/app/make-post/make-post.component.ts
+++ angular/src/app/make-post/make-post.component.ts
@@ -15,38 +15,6 @@ export interface Category{
 })
 export class MakePostComponent implements OnInit {
 
-  postForm = new FormGroup({
-    title : new FormControl(''),
-    body : new FormControl(''),
-    category: new FormControl([])
-  });
-
-  selectable = true;
-  removable = true;
-  addOnBlur = true;
-  readonly separatorKeysCodes = [ENTER, COMMA] as const;
-  categories: Category[] = [
-    {name: 'Computer Science'},
-    {name: 'Science'},
-    {name: 'Math'}
-  ];
-
-  add(event: MatChipInputEvent): void {
-    const value = (event.value || '').trim();
-    //add category
-    if (value) {
-      this.categories.push({name: value});
-    }
-    //clear input
-    event.chipInput!.clear();
-  }
-
-  remove(category: Category): void{
-    const index = this.categories.indexOf(category);
-    if (index >= 0) {
-      this.categories.splice(index, 1);
-    }
-  }
   constructor() { }
 
   ngOnInit(): void {
