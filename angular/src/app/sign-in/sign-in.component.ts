db32687a8dbc388929eb670e325875089d473900
Samantha Hallcox <shallc1@students.towson.edu>
2021-11-26 20:48:00 -0500

    Added banner

--- angular/src/app/sign-in/sign-in.component.ts
+++ angular/src/app/sign-in/sign-in.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
+  pageTitle:string = "Sign In";
 
   constructor() { }
 
