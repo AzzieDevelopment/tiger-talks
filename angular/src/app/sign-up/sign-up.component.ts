119d8552466a40666797faceb20f5468c503c862
Samantha Hallcox <shallc1@students.towson.edu>
2021-11-27 14:58:57 -0500

    Added client side form validation and redirection on submit

--- angular/src/app/sign-up/sign-up.component.ts
+++ angular/src/app/sign-up/sign-up.component.ts
@@ -1,16 +1,100 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IUser } from '../models/user';
+import { RegisterService } from '../services/register.service';
 
 @Component({
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css']
 })
-export class SignUpComponent implements OnInit {
+export class SignUpComponent implements OnInit, OnDestroy {
   pageTitle: string = "Sign Up";
-  signUpUserData = {}
+  signUpData:any = { 
+    acceptTerms: false
+  }
+  passwordsMatch = true;
+  validEmail = true;
+  subscription: any;
   
-  constructor() { }
+  constructor(
+    private registerService: RegisterService,
+    private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
+  validatePasswords() {
+    if (this.signUpData?.verifyPassword !== this.signUpData?.password) {
+      this.passwordsMatch = false;
+    } else {
+      this.passwordsMatch = true;
+    }
+  }
+
+  validateEmail() {
+    let pattern = new RegExp('^[A-Za-z0-9._%+-]+(@towson.edu|@students.towson.edu)$');
+    this.validEmail = pattern.test(this.signUpData.email);
+  }
+
+  togglePassword() {
+    let icon = document.getElementById('togglePassword');
+    let password = document.getElementById('password');
+
+    // switch input type
+    const type = password?.getAttribute('type') === 'password' ? 'text' : 'password';
+    password?.setAttribute('type', type);
+
+    // toggle icon slash
+    icon?.classList.toggle('bi-eye');
+  }
+
+  toggleVerify() {
+    let icon = document.getElementById('toggleVerify');
+    let password = document.getElementById('verifyPassword');
+
+    // switch input type
+    const type = password?.getAttribute('type') === 'password' ? 'text' : 'password';
+    password?.setAttribute('type', type);
+    
+    // toggle icon slash
+    icon?.classList.toggle('bi-eye');
+  }
+
+  onSubmit() {
+    let user: IUser = this.getUserData();
+
+    this.subscription = this.registerService.register(user).subscribe(
+      data => {
+        console.log('Success!', data);
+      },
+      error => {
+        if (error.status === 403) {
+          alert('User with this Net ID or Email already exists');
+        }
+      }
+    );
+    // redirect to sign in page
+    this.router.navigate(['signin']);
+  }
+
+  private getUserData(): IUser {
+    console.log('[', this.signUpData.bio.trim(), ']');
+    return {
+      Id: this.signUpData.netID,
+      FirstName: this.signUpData.firstName,
+      LastName: this.signUpData.lastName,
+      Email: this.signUpData.email,
+      UserType: 1,
+      Permission: 1,
+      Bio: this.signUpData.bio.trim(),
+      PreferredName: this.signUpData.preferredName,
+      Pronouns: this.signUpData.pronouns,
+      Password: this.signUpData.password
+    };
   }
 
 }
