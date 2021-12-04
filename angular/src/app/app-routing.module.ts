44cbeddfa3ade39586e68efdeb32e7de06725568
Samantha Hallcox <shallc1@students.towson.edu>
2021-12-03 22:48:46 -0500

    Update routes

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -9,25 +9,35 @@ import { PageNotFoundComponent } from './errors/pagenotfound/pagenotfound.compon
 import { TigerSpacesGridResolverService } from './tigerspaces-grid/tigerspaces-grid-resolver.service';
 import { TigerSpacePageComponent } from './tigerspace-page/tigerspace-page.component';
 import { AuthGuard } from './guards/auth.guard';
+import { RecentPostsResolverService } from './home-page/recent-posts/recent-posts-resolver.service';
+import { TigerSpaceResolverService } from './tigerspace-page/tigerspace-page-resolver.service';
+import { TigerspacePostsResolverService } from './tigerspace-page/tigerspace-posts-resolver.service';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
-  { path: 'home', component: HomePageComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { 
+    path: 'home', component: HomePageComponent,
+    resolve: { recentPosts: RecentPostsResolverService } 
+  },
   { path: 'signup', component: SignUpComponent },
   { path: 'signin', component: SignInComponent },
   { path: 'guidelines', component: GuidelinesComponent},
-  { path: 'tigerpage', component: TigerSpacePageComponent },
   { 
     path: 'tigerspaces', 
     component: TigerSpacesGridComponent,
     canActivate: [AuthGuard], // TODO: remove
     resolve: { tigerspaces: TigerSpacesGridResolverService }
   },
-  { path: 'tigerspaces/:id', component: TigerSpacePageComponent },
+  { 
+    path: 'tigerspace/:id', 
+    component: TigerSpacePageComponent,
+    resolve: { 
+      tigerspace: TigerSpaceResolverService,
+      posts: TigerspacePostsResolverService
+     }
+  },
   { path: '404', component: PageNotFoundComponent },
-  { path: '**', component: PageNotFoundComponent },
-  { path: 'tigerspaces', component: TigerSpacesGridComponent },
-  { path: '**', redirectTo: '' }
+  { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
