d03779cea4af8006dcda89ec90204868bd3ca377
Samantha Hallcox <shallc1@students.towson.edu>
2021-12-04 01:30:16 -0500

    Update comment interface and service

--- angular/src/app/services/comment.service.ts
+++ angular/src/app/services/comment.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IComment } from '../models/comment';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,8 @@ import { Injectable } from '@angular/core';
 export class CommentService {
 
   constructor() { }
+
+  // getComments(postId: number): Observable<IComment[]> {
+
+  // }
 }
