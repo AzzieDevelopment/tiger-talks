5126eeee0578c5e29bf7edd9ff9f55087aee2f5f
Samantha Hallcox <shallc1@students.towson.edu>
2021-12-03 22:52:32 -0500

    Add post service functions

--- angular/src/app/services/post.service.ts
+++ angular/src/app/services/post.service.ts
@@ -8,11 +8,26 @@ import { IPost } from '../models/post';
 })
 export class PostService {
 
-  _recentPostsUrl = "/api/getrecentposts";
+  _recentPostsUrl = '/api/viewRecentPosts';
+  _getPostsBaseUrl = '/api/getposts'; // for tiger space posts
+  _getRecentPostsUserInfoUrl = 'api/getRecentPostsUserInfo';
+  _getNumCommentsBaseURL = '/api/commentcount/';
 
   constructor(private httpClient: HttpClient) { }
 
   getRecentPosts(): Observable<IPost[]> {
     return this.httpClient.get<IPost[]>(this._recentPostsUrl);
+  }
+
+  getRecentPostsUserInfo(): Observable<any> {
+    return this.httpClient.get<any>(this._getRecentPostsUserInfoUrl);
+  }
+
+  getTigerSpacePosts(tigerSpaceId: number): Observable<IPost[]> {
+    return this.httpClient.get<IPost[]>(`${this._getPostsBaseUrl}/${tigerSpaceId}`);
+  }
+
+  getNumberOfComments(postId: number): Observable<any> {
+    return this.httpClient.get<any>(`${this._getNumCommentsBaseURL}/${postId}`);
   }
 }
