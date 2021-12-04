866a38af7d67117d814dd827b388cf4cba535187
Samantha Hallcox <shallc1@students.towson.edu>
2021-12-03 22:51:03 -0500

    Update recent posts, add resolver

--- angular/src/app/home-page/home-page.component.ts
+++ angular/src/app/home-page/home-page.component.ts
@@ -1,16 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IPost } from '../models/post';
+import { IUser } from '../models/user';
 
 @Component({
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit, OnDestroy {
   
   pageTitle = "Welcome to Tiger Talks!";
+  recentPosts: IPost[] = [];
+  subscription: any;
   
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.subscription = this.activatedRoute.data.subscribe(
+      data => {
+        this.recentPosts = data.recentPosts;
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
--- angular/src/app/home-page/recent-posts/recent-posts.component.ts
+++ angular/src/app/home-page/recent-posts/recent-posts.component.ts
@@ -1,53 +1,24 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { IPost } from 'src/app/models/post';
-import { PostService } from 'src/app/services/post.service';
-
-// In preparation for backend changes
-// constructor(
-//   public Id: number,
-//   public Title: string, //(for post)
-//   public Body: string,
-//   public Category: string,
-//   public Upvotes: number,
-//   public Timestamp: string, // The backend should reformat this to look presentable
-//   public UserId: string,
-//   public TigerSpaceId: number,
-//   public Pronouns: string,
-//   public Major : string
-//   public Title : string //(for tiger spaces)
-//   public Comments: number //(amount of comments)
-//
-// ) {}
-//}
 
 @Component({
   selector: 'recent-posts',
   templateUrl: './recent-posts.component.html',
   styleUrls: ['./recent-posts.component.css']
 })
-export class RecentPostsComponent implements OnInit, OnDestroy {
+export class RecentPostsComponent implements OnInit {
 
-  posts: IPost[] = [];
-  subscription: any;
+  @Input() posts: IPost[] = [];
 
   constructor(
-    private router:Router, 
-    private postService: PostService) { }
+    private router:Router) { }
 
   commentRedirect(postId: any){
     this.router.navigate([`comment/${postId}`]);
   }
 
   ngOnInit(): void {
-    this.subscription = this.postService.getRecentPosts().subscribe(
-      res => this.posts = res,
-      err => console.log(err)
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
   }
 
 }
