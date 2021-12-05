66f2b508c84c16546fa852fbde479d2ae9b48b90
Samantha Hallcox <shallc1@students.towson.edu>
2021-12-05 16:17:53 -0500

    Add functions to get post comments and create comment

--- angular/src/app/services/comment.service.ts
+++ angular/src/app/services/comment.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IComment } from '../models/comment';
@@ -7,9 +8,18 @@ import { IComment } from '../models/comment';
 })
 export class CommentService {
 
-  constructor() { }
+  private _getPostCommentsBaseUrl = '/api/getpostcomments';
+  private _createCommentUrl = '/api/createComment';
 
-  // getComments(postId: number): Observable<IComment[]> {
+  constructor(private httpClient: HttpClient) { }
 
-  // }
+  // get comments for a specific post
+  getPostComments(postId: number): Observable<IComment[]> {
+    return this.httpClient.get<IComment[]>(`${this._getPostCommentsBaseUrl}/${postId}`);
+  }
+
+  // create comment
+  createComment(comment: IComment) {
+    return this.httpClient.post<any>(this._createCommentUrl, comment);
+  }
 }
